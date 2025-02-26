f0c5c7782a8b7223ce73cfd159d63d3177e9616b
aria.aman <ariaaman@outlook.fr>
2025-02-27 00:42:25 +0100

    Update de la page cennik et ajout d'image

--- pages/Cennik.vue
+++ pages/Cennik.vue
@@ -20,86 +20,130 @@
         </div>
 
         <div class="container-prix">
-            <!-- Sous‑titre + ligne -->
+            <!-- Sous-titre -->
             <div class="subheader">
                 <h2>TWOJA INWESTYCJA W ZDROWY UŚMIECH:</h2>
             </div>
 
-            <!-- Tableau des catégories / items / prix -->
+            <!-- TABLEAU DES TARIFS -->
             <div class="pricing-table">
-                <!-- Bloc KONSULTACJE -->
+                <!-- KONSULTACJE -->
                 <div class="pricing-block">
                     <h3>KONSULTACJE</h3>
-                    <div class="pricing-row">
+                    <div class="pricing-row-name">
                         <div class="item-name">Konsultacja stomatologiczna</div>
-                        <div class="item-price">150 zł</div>
-                    </div>
-                    <div class="pricing-row">
                         <div class="item-name">
                             Konsultacja stomatologiczna ze zdjęciem pantomograficznym cyfrowym
                         </div>
-                        <div class="item-price">200 zł</div>
-                    </div>
-                    <div class="pricing-row">
                         <div class="item-name">Konsultacja implantologiczna z tomografem</div>
+                    </div>
+                    <div class="pricing-row-price">
+                        <div class="item-price">200 zł</div>
+                        <div class="item-price">150 zł</div>
                         <div class="item-price">400 zł</div>
                     </div>
                 </div>
 
-                <!-- Bloc DIAGNOSTYKA -->
+                <!-- DIAGNOSTYKA -->
                 <div class="pricing-block">
                     <h3>DIAGNOSTYKA</h3>
-                    <div class="pricing-row">
+                    <div class="pricing-row-name">
                         <div class="item-name">Zdjęcie punktowe</div>
-                        <div class="item-price">50 zł</div>
-                    </div>
-                    <div class="pricing-row">
                         <div class="item-name">Zdjęcie pantomograficzne</div>
-                        <div class="item-price">100 zł</div>
-                    </div>
-                    <div class="pricing-row">
                         <div class="item-name">Tomograf</div>
+                    </div>
+                    <div class="pricing-row-price">
+                        <div class="item-price">50 zł</div>
+                        <div class="item-price">100 zł</div>
                         <div class="item-price">250–350 zł</div>
                     </div>
                 </div>
 
-                <!-- Bloc PROFILAKTYKA -->
+                <!-- PROFILAKTYKA -->
                 <div class="pricing-block">
                     <h3>PROFILAKTYKA</h3>
-                    <div class="pricing-row">
+                    <div class="pricing-row-name">
                         <div class="item-name">
                             Pakiet higienizacyjny (skaling, piaskowanie, fluoryzacja)
                         </div>
-                        <div class="item-price">350 zł</div>
-                    </div>
-                    <div class="pricing-row">
                         <div class="item-name">Fluoryzacja obu łuków zębowych</div>
-                        <div class="item-price">150 zł</div>
-                    </div>
-                    <div class="pricing-row">
                         <div class="item-name">Piaskowanie zębów</div>
+                        <div class="item-name">Skaling ultradźwiękowy</div>
+                    </div>
+                    <div class="pricing-row-price">
+                        <div class="item-price">350 zł</div>
+                        <div class="item-price">150 zł</div>
+                        <div class="item-price">200 zł</div>
                         <div class="item-price">200 zł</div>
                     </div>
-                    <div class="pricing-row">
-                        <div class="item-name">Skaling ultradźwiękowy</div>
-                        <div class="item-price">200 zł</div>
+                </div>
+
+                <!-- IMPLANTOLOGIA -->
+                <div class="pricing-block">
+                    <h3>IMPLANTOLOGIA</h3>
+                    <div class="pricing-row-name">
+                        <div class="item-name">Wszczepienie implantu</div>
+                        <div class="item-name">Odsłonięcie implantu i założenie śruby gojącej</div>
+                        <div class="item-name">Korona porcelanowa na implancie</div>
+                        <div class="item-name">
+                            Prace implantologiczne złożone wyceniane są indywidualnie
+                        </div>
+                        <div class="item-name">Szablon implantologiczny</div>
+                        <div class="item-name">
+                            Podniesienie dna zatoki szczękowej (sinus lift)
+                        </div>
+                    </div>
+                    <div class="pricing-row-price">
+                        <div class="item-price">3000 zł</div>
+                        <div class="item-price">300 zł</div>
+                        <div class="item-price">od 3000 zł</div>
+                        <div class="item-price">-</div>
+                        <div class="item-price">1000 zł</div>
+                        <div class="item-price">3000–4000 zł</div>
+                    </div>
+                </div>
+
+                <!-- PROTETYKA -->
+                <div class="pricing-block">
+                    <h3>PROTETYKA</h3>
+                    <div class="pricing-row-name">
+                        <div class="item-name">Implantologia dentystyczna</div>
+                        <div class="item-name">Implantologia dentystyczna</div>
+                        <div class="item-name">Implantologia dentystyczna</div>
+                    </div>
+                    <div class="pricing-row-price">
+                        <div class="item-price">Implantologia dentystyczna</div>
+                        <div class="item-price">Implantologia dentystyczna</div>
+                        <div class="item-price">Implantologia dentystyczna</div>
+                    </div>
+                </div>
+
+                <!-- ENDODONCJA (MIKROSKOPOWA) -->
+                <div class="pricing-block">
+                    <h3>ENDODONCJA (MIKROSKOPOWA)</h3>
+                    <div class="pricing-row-name">
+                        <div class="item-name">Implantologia dentystyczna</div>
+                        <div class="item-name">Implantologia dentystyczna</div>
+                        <div class="item-name">Implantologia dentystyczna</div>
+                    </div>
+                    <div class="pricing-row-price">
+                        <div class="item-price">Implantologia dentystyczna</div>
+                        <div class="item-price">Implantologia dentystyczna</div>
+                        <div class="item-price">Implantologia dentystyczna</div>
                     </div>
                 </div>
             </div>
         </div>
-
-
-
     </section>
 </template>
 
 <style scoped>
 .container-prix {
-    max-width: 1200px;
     display: flex;
     width: 100%;
     flex-direction: column;
     margin: 0;
+    padding-bottom: 96px;
 }
 
 /* Couleur de fond bleu sombre et texte clair */
@@ -109,7 +153,6 @@
     padding: 60px 80px;
     min-height: 100vh;
     font-family: 'Figtree', sans-serif;
-    /* Ajuste la police globale ici */
 }
 
 /* En‑tête : "KOSZTY LECZENIA" et paragraphe explicatif */
@@ -118,10 +161,6 @@
     gap: 526px;
     height: 448px;
     margin-bottom: 40px;
-}
-
-.title-left {
-    flex-shrink: 0;
 }
 
 .main-title {
@@ -174,35 +213,53 @@
 .pricing-table {
     display: flex;
     flex-direction: column;
-    gap: 40px;
 }
 
 /* Chaque bloc (KONSULTACJE, DIAGNOSTYKA...) */
 .pricing-block {
     border-top: 1px solid #FFFFFF;
-    /* Fine ligne de séparation */
-    padding-top: 20px;
+    width: 1700px;
+    gap: 182px;
+    display: flex;
+    padding: 32px 0px;
+    align-items: center;
 }
 
 .pricing-block h3 {
     font-family: 'Aboreto', cursive;
-    font-size: 24px;
-    color: #BC9667;
-    margin: 0 0 20px;
+    font-size: 40px;
+    color: #fff;
+    line-height: 56px;
+    width: 30%;
+    display: flex;
     font-weight: 400;
     text-transform: uppercase;
-    /* Comme sur la maquette */
+    align-items: center;
 }
 
 /* Chaque ligne : nom de l'acte à gauche + prix à droite */
-.pricing-row {
+.pricing-row-name {
     display: flex;
+    gap: 32px;
+    width: 428px;
+    height: 100%;
+    flex-direction: column;
+}
+
+.pricing-row-price {
+    display: flex;
+    /* width: 428px; */
+    /* height: calc(100% + 100px); */
+    position: relative;
+    gap: 40px;
+    flex-direction: column;
     justify-content: space-between;
-    margin-bottom: 10px;
+    align-items: flex-start;
 }
 
 /* Nom de l'acte / du service */
 .item-name {
+    font-family: 'Figtree', cursive;
     font-size: 20px;
     line-height: 32px;
     color: #fff;
